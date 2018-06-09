47876af1d4914067cc7554b644e35866912fa209
Stanislav Pankevich <s.pankevich@gmail.com>
2018-06-09 21:11:34 +0200

    Better CMake structure to integrate it easier into Mull.

---
+++ llvm-jit-objc/llvm-jit-objc/tests/SandboxTest.cpp
@@ -0,0 +1,260 @@
+  //#include "ObjCEnabledMemoryManager.h"
+
+#include "ObjCEnabledMemoryManager.h"
+#include "ObjCResolver.h"
+#include "ObjCRuntime.h"
+#include "TestHelpers.h"
+#include "ObjCRuntimeHelpers.h"
+
+#include <objc/message.h>
+#include <gtest/gtest.h>
+
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/Orc/CompileUtils.h>
+#include <llvm/ExecutionEngine/Orc/IRCompileLayer.h>
+#include <llvm/ExecutionEngine/Orc/LambdaResolver.h>
+#include <llvm/ExecutionEngine/Orc/OrcError.h>
+#include <llvm/ExecutionEngine/Orc/RTDyldObjectLinkingLayer.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/Instructions.h>
+#include <llvm/IR/CallSite.h>
+#include <llvm/Support/DynamicLibrary.h>
+#include <llvm/Support/raw_ostream.h>
+#include <llvm/Support/TargetSelect.h>
+
+#include <fstream>
+
+using namespace llvm;
+using namespace llvm::orc;
+
+static
+llvm::orc::RTDyldObjectLinkingLayer::MemoryManagerGetter getMemoryManager() {
+  llvm::orc::RTDyldObjectLinkingLayer::MemoryManagerGetter GetMemMgr =
+  []() {
+    return std::make_shared<SectionMemoryManager>();
+  };
+  return GetMemMgr;
+}
+
+namespace SwiftDyLibPath {
+  static const char *const Core =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftCore.dylib";
+  static const char *const Darwin =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftDarwin.dylib";
+  static const char *const ObjectiveC =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftObjectiveC.dylib";
+  static const char *const Dispatch =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftDispatch.dylib";
+  static const char *const CoreFoundation =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftCoreFoundation.dylib";
+  static const char *const IOKit =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftIOKit.dylib";
+  static const char *const CoreGraphics =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftCoreGraphics.dylib";
+
+    //  -load=/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftFoundation.dylib
+    //  -load=/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftXPC.dylib
+
+  static const char *const Foundation =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftFoundation.dylib";
+  static const char *const CoreData =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftCoreData.dylib";
+
+  static const char *const XPC =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftXPC.dylib";
+  static const char *const os =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftos.dylib";
+  static const char *const Metal =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftMetal.dylib";
+
+  static const char *const CoreImage =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftCoreImage.dylib";
+  static const char *const QuartzCore =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftQuartzCore.dylib";
+  static const char *const AppKit =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftAppKit.dylib";
+
+  static const char *const XCTest =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftXCTest.dylib";
+  static const char *const SwiftOnoneSupport =
+  "/Applications/Xcode-9.2.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/macosx/libswiftSwiftOnoneSupport.dylib";
+
+
+}
+
+static void loadSwiftLibrariesOrExit() {
+  const char *const libraries[] = {
+    SwiftDyLibPath::Core,
+    SwiftDyLibPath::Darwin,
+    SwiftDyLibPath::ObjectiveC,
+    SwiftDyLibPath::Dispatch,
+    SwiftDyLibPath::CoreFoundation,
+    SwiftDyLibPath::IOKit,
+    SwiftDyLibPath::CoreGraphics,
+    SwiftDyLibPath::Foundation,
+    SwiftDyLibPath::CoreData,
+
+    SwiftDyLibPath::XPC,
+    SwiftDyLibPath::os,
+    SwiftDyLibPath::Metal,
+    SwiftDyLibPath::CoreImage,
+    SwiftDyLibPath::QuartzCore,
+    SwiftDyLibPath::AppKit,
+
+    SwiftDyLibPath::XCTest,
+    SwiftDyLibPath::SwiftOnoneSupport,
+  };
+
+  for (const char *const lib: libraries) {
+    std::string errorMessage;
+    if (sys::DynamicLibrary::LoadLibraryPermanently(lib,
+                                                    &errorMessage)) {
+      errs() << errorMessage << "\n";
+      exit(1);
+    }
+  }
+}
+
+#include <dirent.h>
+
+std::vector<std::string> loadBitcodeFileListFromPath(std::string path) {
+  std::vector<std::string> bitcodePaths;
+
+  std::ifstream ifs(path);
+
+  for (std::string path; getline(ifs, path); ) {
+    if (path.at(0) == '#') {
+      continue;
+    }
+
+    bitcodePaths.push_back(path);
+  }
+
+  assert(bitcodePaths.size() > 0);
+
+  return bitcodePaths;
+}
+
+static void createDummyFunction(StringRef Name, llvm::Module &M) {
+   auto &Context = M.getContext();
+   Function *F = cast<Function>(M.getOrInsertFunction(
+     Name, FunctionType::get(Type::getVoidTy(Context), false)));
+   BasicBlock *BB = BasicBlock::Create(Context, "entry", F);
+  new UnreachableInst(Context, BB);
+}
+
+TEST(DISABLED_Sandbox, Test) {
+    // These lines are needed for TargetMachine TM to be created correctly.
+  llvm::InitializeNativeTarget();
+  llvm::InitializeNativeTargetAsmPrinter();
+  llvm::InitializeNativeTargetAsmParser();
+
+  llvm::sys::DynamicLibrary::LoadLibraryPermanently(nullptr);
+
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+                                                      "/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation"
+                                                      ));
+  assert(!sys::DynamicLibrary::LoadLibraryPermanently(
+                                                      "/Applications/Xcode-9.2.app/Contents/Developer/Platforms/MacOSX.platform/Developer/Library/Frameworks/XCTest.framework/XCTest"
+                                                      ));
+  loadSwiftLibrariesOrExit();
+
+  llvm::LLVMContext llvmContext;
+
+  RTDyldObjectLinkingLayer objectLayer(getMemoryManager());
+
+  std::unique_ptr<TargetMachine> TM(
+                                    EngineBuilder().selectTarget(llvm::Triple(),
+                                                                 "",
+                                                                 "",
+                                                                 SmallVector<std::string, 1>())
+                                    );
+
+  TM->setOptLevel(CodeGenOpt::Level::None);
+
+  assert(TM.get());
+  SimpleCompiler compiler(*TM);
+
+  std::shared_ptr<ObjCResolver> objcResolver = std::make_shared<ObjCResolver>();
+
+  std::vector<std::string> bitcodeFilePaths = loadBitcodeFileListFromPath("/opt/bitcode-targets/swift-protobuf-bitcode/bitcode.list.yml");
+
+  auto resolver = llvm::orc::createLambdaResolver(
+    [&](std::string name) {
+      if (JITSymbol js = objectLayer.findSymbol(name, false)) {
+        return js;
+      }
+
+      if (auto SymAddr = RTDyldMemoryManager::getSymbolAddressInProcess(name)) {
+        return JITSymbol(SymAddr, JITSymbolFlags::Exported);
+      }
+
+      return JITSymbol(nullptr);
+    },
+    [&](std::string name) {
+      return JITSymbol(nullptr);
+    }
+  );
+
+  std::vector<llvm::orc::RTDyldObjectLinkingLayer::ObjHandleT> handles;
+  for (std::string &bitcodeFilePath: bitcodeFilePaths) {
+    errs() << "loading bitcode path: " << bitcodeFilePath << "\n";
+    auto bitcodeModule = loadModuleAtPath(bitcodeFilePath, llvmContext);
+
+    createDummyFunction("dummy", *bitcodeModule);
+
+    RTDyldObjectLinkingLayer::ObjectPtr compiledModule =
+      std::make_shared<object::OwningBinary<object::ObjectFile>>(compiler(*bitcodeModule));
+    assert(compiledModule);
+    assert(compiledModule->getBinary());
+    assert(compiledModule->getBinary()->isMachO());
+
+    llvm::orc::RTDyldObjectLinkingLayer::ObjHandleT handle = objectLayer.addObject(compiledModule, resolver).get();
+    handles.push_back(handle);
+//    Error err = objectLayer.emitAndFinalize(handle);
+//
+//    std::string cacheName("/tmp/_Sandbox.o");
+//    std::error_code EC;
+//    raw_fd_ostream outfile(cacheName, EC, sys::fs::F_None);
+//    outfile.write(compiledModule->getBinary()->getMemoryBufferRef().getBufferStart(),
+//                  compiledModule->getBinary()->getMemoryBufferRef().getBufferSize());
+//    outfile.close();
+  }
+
+  for (uint32_t i = 0; i < handles.size(); i++) {
+    auto &handle = handles.at(i);
+    errs() << "Resolving handle: " << handle->get() << " / " << i << "\n";
+
+    auto symb = (*handle)->getSymbol("_dummy", false);
+    assert(symb.getAddress().get() > 0);
+      //    Error err = objectLayer.emitAndFinalize(handle);
+//    //assert(!err);
+  }
+
+  std::string functionName = "_CustomXCTestRunnerRunAll";
+  JITSymbol symbol = objectLayer.findSymbol(functionName, false);
+
+  void *fpointer =
+  reinterpret_cast<void *>(static_cast<uintptr_t>(symbol.getAddress().get()));
+
+  if (fpointer == nullptr) {
+    errs() << "CustomTestRunner> Can't find pointer to function: "
+    << functionName << "\n";
+    exit(1);
+  }
+
+  auto runnerFunction = ((int (*)(void))(intptr_t)fpointer);
+
+  int result = runnerFunction();
+  EXPECT_EQ(result, 0);
+
+//  void *runnerPtr = sys::DynamicLibrary::SearchForAddressOfSymbol("CustomXCTestRunnerRunAll");
+//  auto runnerFPtr = ((int (*)(void))runnerPtr);
+//  if (runnerFPtr == nullptr) {
+//    errs() << "Could not find CustomXCTestRunner function: CustomXCTestRunnerRunAll()" << "\n";
+//    exit(1);
+//  }
+//  int result = runnerFPtr();
+//  ASSERT_EQ(result, 0);
+}
+
